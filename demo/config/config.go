b3ca5d665619d5368b2bdb08b9bce96b6cf3ded4
Minke Zhang <mzhang@blogzhang.com>
2022-06-27 14:53:53 -0700

    Intermediate commit to implement config.Step

--- demo/config/config.go
+++ demo/config/config.go
@@ -2,16 +2,14 @@ package config
 
 import (
 	"encoding/json"
+	"math"
 
 	"github.com/downflux/go-boids/agent"
 	"github.com/downflux/go-boids/internal/geometry/2d/vector/polar"
 	"github.com/downflux/go-geometry/2d/vector"
 )
 
-var _ agent.A = &A{}
-
-// TODO(minkezhang): Implement A.Step method.
-// var _ agent.RW = &A{}
+var _ agent.RW = &A{}
 
 type O struct {
 	P            vector.V
@@ -38,6 +36,23 @@ func (a *A) Mass() float64         { return a.O.Mass }
 func (a *A) MaxVelocity() polar.V  { return a.O.MaxVelocity }
 func (a *A) MaxNetTorque() float64 { return a.O.MaxNetTorque }
 func (a *A) MaxNetForce() float64  { return a.O.MaxNetForce }
+
+// Step advances the Boid simulation by a single step.
+//
+// The implemented agent will turn in place if the magnitude of the velocity is
+// zero.
+//
+// We could alteratively instruct the agent to reverse if the angle is too
+// large.
+//
+// The angular component of the input vector is relative to the agent heading.
+func (a *A) Step(v polar.V, tau float64) {
+	a.O.Heading = *polar.New(1, a.O.Heading.Theta()+tau*math.Copysign(
+		math.Min(a.MaxVelocity().Theta(), math.Abs(v.Theta())), v.Theta(),
+	))
+
+	a.O.P = vector.Add(a.P(), vector.Scale(tau, a.V()))
+}
 
 func (a *A) SetP(p vector.V)      { a.O.P = p }
 func (a *A) SetV(v vector.V)      { a.O.V = v }

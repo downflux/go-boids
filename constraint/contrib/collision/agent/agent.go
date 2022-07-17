c0db932cbd81fd4ceb2efac8e3e5598dc2a7b4d7
Minke Zhang <minke.zhang@gmail.com>
2022-07-17 01:55:42 -0700

    Add initial commit for preemptive collision constraint

--- constraint/contrib/collision/agent/agent.go
+++ constraint/contrib/collision/agent/agent.go
@@ -1,9 +1,15 @@
 package agent
 
 import (
+	"math"
+	"math/rand"
+
 	"github.com/downflux/go-boids/agent"
 	"github.com/downflux/go-boids/constraint"
+	"github.com/downflux/go-geometry/2d/hypersphere"
+	"github.com/downflux/go-geometry/2d/line"
 	"github.com/downflux/go-geometry/2d/vector"
+	"github.com/downflux/go-geometry/epsilon"
 )
 
 var _ constraint.C = C{}
@@ -21,5 +27,42 @@ func New(o O) *C {
 		o: o,
 	}
 }
+func jitter() vector.V {
+	return vector.Unit(
+		*vector.New(
+			rand.Float64()+1e-5,
+			rand.Float64()+1e-5,
+		),
+	)
+}
+func (c C) Accelerate(a agent.RO) vector.V {
+	v := vector.Add(a.V(), c.o.Obstacle.V())
+	d := *hypersphere.New(c.o.Obstacle.P(), c.o.Obstacle.R()+a.R())
 
-func (c C) Accelerate(a agent.RO) vector.V { return vector.Sub(a.P(), c.o.Obstacle.P()) }
+	l := line.New(a.P(), v)
+	if pmin, pmax, ok := l.IntersectCircle(d); ok {
+		t := math.Inf(0)
+		var p vector.V
+		for _, pc := range []vector.V{pmin, pmax} {
+			if tc := l.T(pc); tc >= 0 && tc <= 1 && tc < t {
+				t = tc
+				p = pc
+			}
+		}
+
+		if p != nil {
+			q := vector.Sub(l.L(t), c.o.Obstacle.P())
+			if epsilon.Within(vector.Magnitude(p), 0) {
+				q = jitter()
+			}
+
+			separation := vector.Magnitude(q) / d.R()
+			return vector.Scale(
+				math.Max(1e-5, 1/(separation-d.R())),
+				vector.Unit(vector.Sub(p, c.o.Obstacle.P())),
+			)
+		}
+	}
+
+	return *vector.New(0, 0)
+}

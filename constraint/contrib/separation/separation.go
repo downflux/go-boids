92ec9e5263492731b77fe52aa84d7cfaf3fcbb45
Minke Zhang <mzhang@blogzhang.com>
2023-01-07 14:32:07 -0800

    Add separation

---
+++ constraint/contrib/separation/separation.go
@@ -0,0 +1,52 @@
+package separation
+
+import (
+	"github.com/downflux/go-boids/constraint"
+	"github.com/downflux/go-boids/constraint/contrib/seek"
+	"github.com/downflux/go-database/agent"
+	"github.com/downflux/go-database/database"
+	"github.com/downflux/go-database/filters"
+	"github.com/downflux/go-geometry/2d/vector"
+	"github.com/downflux/go-geometry/nd/hyperrectangle"
+
+	vnd "github.com/downflux/go-geometry/nd/vector"
+)
+
+func Separation(db *database.DB, r float64) constraint.Accelerator {
+	return func(a agent.RO) vector.V {
+		x, y := a.Position().X(), a.Position().Y()
+		aabb := *hyperrectangle.New(
+			vnd.V{x - r, y - r}, vnd.V{x + r, y + r},
+		)
+
+		obstacles := db.QueryAgents(aabb, func(b agent.RO) bool {
+			// QueryAgents is an AABB query function -- we will
+			// manually filter out agents which lie within the AABB
+			// but outside the circle.
+			if d := vector.Magnitude(vector.Sub(a.Position(), b.Position())); d > r {
+				return false
+			}
+			return a.ID() != b.ID() && filters.AgentIsTeammate(a, b) && !filters.AgentOnDifferentLayers(a, b)
+		})
+
+		// Calculate the weighted average of the neighboring positions.
+		// This value is our center-of-mass for the the entire group
+		// (i.e. the desired velocity).  Therefore, we need to wrap this
+		// in the steering constraint.
+		sum := 0.0
+		weights := make([]float64, 0, len(obstacles))
+		vs := make([]vector.V, 0, len(obstacles))
+		for _, o := range obstacles {
+			m := o.Mass()
+			sum += m
+			weights = append(weights, m)
+			vs = append(vs, vector.Sub(a.Position(), o.Position()))
+		}
+
+		buf := vector.M{0, 0}
+		for i, w := range weights {
+			buf.Add(vector.Scale(w/sum, vs[i]))
+		}
+		return seek.SLSDO(buf.V())(a)
+	}
+}
